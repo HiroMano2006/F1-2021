a67163e0d47555a4cb4e3803a56328889775d6f0
Hiro Mano <25manohi@asij.ac.jp>
2021-05-19 12:23:43 +0900

    Initial Commit

---
+++ F1 2021/ViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  F1 2021
+//
+//  Created by Hiroaki Mano on 2021/05/11.
+//
+
+import UIKit
+import SafariServices
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func WhenR1ButtonPushed(_ sender: UIButton) {
+        performSegue(withIdentifier: "Round1", sender: nil)
+    }
+    
+    @IBAction func WhenR2ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round2", sender: nil)
+    }
+    
+    @IBAction func WhenR3ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round3", sender: nil)
+    }
+    
+    @IBAction func WhenR4ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round4", sender: nil)
+    }
+    
+    @IBAction func WhenR5ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round5", sender: nil)
+    }
+    
+    @IBAction func WhenR6ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round6", sender: nil)
+    }
+    
+    @IBAction func WhenR7ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round7", sender: nil)
+    }
+    
+    @IBAction func WhenR8ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round8", sender: nil)
+    }
+    
+    @IBAction func WhenR9ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round9", sender: nil)
+    }
+    
+    @IBAction func WhenR10ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round10", sender: nil)
+    }
+    
+    @IBAction func WhenR11ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round11", sender: nil)
+    }
+    
+    @IBAction func WhenR12ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round12", sender: nil)
+    }
+    
+    @IBAction func WhenR13ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round13", sender: nil)
+    }
+    
+    @IBAction func WhenR14ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round14", sender: nil)
+    }
+    
+    @IBAction func WhenR15ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round15", sender: nil)
+    }
+    
+    @IBAction func WhenR16ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round16", sender: nil)
+    }
+    
+    @IBAction func WhenR17ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Roudn17", sender: nil)
+    }
+    
+    @IBAction func WhenR18ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round18", sender: nil)
+    }
+    
+    @IBAction func WhenR19ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round19", sender: nil)
+    }
+    
+    @IBAction func WhenR20ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round20", sender: nil)
+    }
+    
+    @IBAction func WhenR21ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round21", sender: nil)
+    }
+    
+    @IBAction func WhenR22ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round22", sender: nil)
+    }
+    
+    @IBAction func WhenR23ButtonPushed(_ sender: Any) {
+        performSegue(withIdentifier: "Round23", sender: nil)
+    }
+    
+    @IBAction func OnF1ButtonPushed(_ sender: Any) {
+        
+        
+    }
+    
+}
+
